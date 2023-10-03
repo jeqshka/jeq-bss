4f9f9f750752cb963d609e0b12e6926f916e306c
jeqshka <146786273+jeqshka@users.noreply.github.com>
2023-10-03 20:24:12 +0500

    Create jeqbee.lua

---
+++ jeqbee.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 1537690962 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Boxking776/kocmoc/main/games/bss.lua"))()
+else
+	game.Players.LocalPlayer:Kick("Ошибка.\n\nЗайдите в BSS симулятор!")
+end
