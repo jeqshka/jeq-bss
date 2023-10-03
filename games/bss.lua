a8160c25b2e057d57d81d802de04bccb49e9dede
jeqshka <146786273+jeqshka@users.noreply.github.com>
2023-10-03 20:41:35 +0500

    Update bss.lua

--- games/bss.lua
+++ games/bss.lua
@@ -1,1 +1,9 @@
+repeat wait(0.1) until game:IsLoaded()
 
+getgenv().Star = "⭐"
+getgenv().Danger = "⚠️"
+getgenv().ExploitSpecific = "📜"
+
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/jeqshka/jeq-bss/main/liblary.lua"))()
+getgenv().api = loadstring(game:HttpGet("https://raw.githubusercontent.com/jeqshka/jeq-bss/main/jequi.lua"))()
+local bssapi = loadstring(game:HttpGet("https://raw.githubusercontent.com/jeqshka/jeq-bss/main/jeqapi.lua"))()
